117c5222a07a5e2a4a37f09a917276ec836c8f86
Menemi <danila.titov1210@yandex.ru>
2023-10-09 23:25:00 +0300

    fix: hotfixes

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -7,28 +7,28 @@ export default function Contacts() {
         <h3 className="highlight-text">Контакты</h3>
         <div className="contacts-list">
           <div className="contacts-unit">
-            <box-icon name="map" size="50px" class="contacts-icon" />
+            <box-icon name="map" size="50px" />
             <div className="contacts-unit-description">
               <h4 className="highlight-text">Местоположение</h4>
               <p>Санкт-Петербург, Россия</p>
             </div>
           </div>
           <a href="https://t.me/Menemi" target="_blanc" className="contacts-unit">
-            <box-icon type="logo" name="telegram" size="50px" class="contacts-icon" />
+            <box-icon type="logo" name="telegram" size="50px" />
             <div className="contacts-unit-description">
               <h4 className="highlight-text">Telegram</h4>
               <p>Menemi</p>
             </div>
           </a>
           <a href="https://github.com/Menemi" target="_blanc" className="contacts-unit">
-            <box-icon type="logo" name="github" size="50px" class="contacts-icon" />
+            <box-icon type="logo" name="github" size="50px" />
             <div className="contacts-unit-description">
               <h4 className="highlight-text">Github</h4>
               <p>Menemi</p>
             </div>
           </a>
           <a href="mailto:danila.titov1210@yandex.ru" target="_blanc" className="contacts-unit">
-            <box-icon name="at" size="50px" class="contacts-icon" />
+            <box-icon name="at" size="50px" />
             <div className="contacts-unit-description">
               <h4 className="highlight-text">Почта</h4>
               <p>Титов Даниил</p>
